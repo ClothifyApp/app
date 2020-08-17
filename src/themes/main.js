8f8a32d4781f3e1540d038ec5f69b38d67328e1b
narias1999 <nicolasarias870@gmail.com>
2020-08-17 15:30:36 -0500

    theme ready

--- src/themes/main.js
+++ src/themes/main.js
@@ -5,8 +5,14 @@ export const mainTheme = {
     white: '#FFFFFF',
     black: '#191919',
     grey: '#4A4A4A',
+    greyLigther: '#C0C0C0',
     pink: '#FC497B',
     gradient: 'linear-gradient(0deg, #B71AB7 0%, #DA0B9F 100%);',
+  },
+  shadows: {
+    small: '0px 2px 4px rgba(26, 22, 32, 0.1)',
+    regular: '0px 4px 8px rgba(26, 22, 32, 0.2);',
+    large: '2px 8px 16px rgba(26, 22, 32, 0.25)',
   },
   fonts: ['Proxima Nova', 'sans-serif', 'Arial'],
   fontSizes: {
