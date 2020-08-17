034a3f3bd2a463ea63a2057c061ee67ebe18fa1e
narias1999 <nicolasarias870@gmail.com>
2020-08-16 23:09:49 -0500

    styled button

--- src/components/base/Button.js
+++ src/components/base/Button.js
@@ -2,19 +2,24 @@ import styled from 'styled-components';
 
 export const Button = styled.button`
   border-radius: 100px;
-  background-color: ${getColor};
+  background: ${getColor};
   color: ${(props) => props.theme.colors.white};
-  padding: 10px 30px;
+  padding: 10px 35px;
+  font-weight: bold;
+  font-size: 18px;
+  border: 0;
+  cursor: pointer;
+  outline: none;
 `;
 
 export const OutlinedButton = styled(Button)`
   border: 2px solid ${getColor};
-  background-color: transparent;
+  background: transparent;
   color: ${getColor};
 `;
 
 function getColor(props) {
   return props.color
     ? props.theme.colors[props.color]
-    : props.theme.colors.primary;
+    : props.theme.colors.gradient;
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {} from '../';
+import { Button } from '../components/base/Button';
 
 export default function Main() {
   return (
     <div>
       <h1>Main View</h1>
+      <Button>Test</Button>
     </div>
   );
 }
