a71b0564d287aa40822466b51d08a849c50c1acf
narias1999 <nicolasarias870@gmail.com>
2020-08-21 18:44:57 -0500

    base styles

--- src/components/Swipe/styles.js
+++ src/components/Swipe/styles.js
@@ -1,3 +1,12 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.section``;
+export const Wrapper = styled.section`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 500px;
+  overflow: hidden;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
--- src/pages/Swipe.js
+++ src/pages/Swipe.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import SwipeComponent from '../components/Swipe';
 
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(100vh - ${({ theme }) => theme.sizes.navbarHeight}px);
+`;
+
 export default function Swipe() {
-  return <SwipeComponent />;
+  return (
+    <Wrapper>
+      <SwipeComponent />
+    </Wrapper>
+  );
 }
